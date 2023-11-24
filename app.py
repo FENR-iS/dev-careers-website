4f4d8aedcab9d3c4b4758c3a60de7f216e8150ab
FENR-iS <151525901+FENR-iS@users.noreply.github.com>
2023-11-24 11:03:15 +0000

    added html and css for basic layout UI and a banner image

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+base_url = "https://dev-careers-website.amitsolankideve.repl.co"
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template("home.html", base_url=base_url)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
